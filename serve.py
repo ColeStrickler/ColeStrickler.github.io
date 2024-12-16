d84b8fb15d39f9b4ce5ab6803d2411301745260c
Cole Strickler <colestrickler@gmail.com>
2024-12-16 17:24:20 -0600

    better serve script

--- serve.py
+++ serve.py
@@ -6,5 +6,11 @@ PORT = 80
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
-    httpd.serve_forever()
+    try:
+        print(f"Serving at http://localhost:{PORT}")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nShutting down server...")
+    finally:
+        httpd.server_close()
+        print("Server closed successfully.")
